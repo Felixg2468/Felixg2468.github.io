420795af8619b3ef73566f2078baa2ba600c20f4
Felixg2468 <104605312+Felixg2468@users.noreply.github.com>
2023-05-01 22:34:58 +0000

    Variables set for coins

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Enter amount");
     int amount = nextInt();
+
+    int quarter = 0;
+    int dime = 0;
+    int nickel = 0;
+    int penny = 0;
+    while (amount > 0){
+      
+    }
       
   }
 }
